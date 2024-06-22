dbf43f1deac51e97dcc9766b48e86d341feec4cf
Prashant Kumar Rajak <106570525+Tranquil02@users.noreply.github.com>
2024-06-22 21:14:56 +0530

    updated

--- contract.sol
+++ contract.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract MyContract {
+    uint public value;
+
+    constructor() {
+        value = 0;
+    }
+
+    // Function to set a value, ensuring the value is greater than zero
+    function set(uint _value) public {
+        require(_value > 0, "Value must be greater than zero");
+        value = _value;
+    }
+
+    // Function to increment the value with an assert check
+    function increment() public {
+        uint newValue = value + 1;
+        assert(newValue > value); 
+        value = newValue;
+    }
+
+    // Function to reset the value and revert the transaction with a custom message
+   // Function to demonstrate the revert() statement
+    function resetValue() public {
+        // Check some condition (in this case, we will always revert)
+        if (value != 0) {
+            revert("Value is not zero, cannot reset.");
+        }
+        value = 0;
+    }
+}
